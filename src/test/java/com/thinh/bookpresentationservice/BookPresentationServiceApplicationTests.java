2a496fe366cd71e153711a35556b0439b6d60802
thinh <anvanthinh@gmail.com>
2025-04-29 00:22:12 +0700

    init

---
+++ src/test/java/com/thinh/bookpresentationservice/BookPresentationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinh.bookpresentationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookPresentationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
